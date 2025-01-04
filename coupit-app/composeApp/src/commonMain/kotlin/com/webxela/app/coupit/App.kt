46c85cb63ab84a001e3afbcbfcb48b6f50d78964
amanverma-765 <akverma4aman@gmail.com>
2025-01-05 02:49:43 +0530

    structure project folders for clean architecture

--- coupit-app/composeApp/src/commonMain/kotlin/com/webxela/app/coupit/App.kt
+++ coupit-app/composeApp/src/commonMain/kotlin/com/webxela/app/coupit/App.kt
@@ -1,9 +1,6 @@
 package com.webxela.app.coupit
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import com.webxela.app.coupit.theme.AppTheme
 
 @Composable
