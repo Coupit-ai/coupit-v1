4ad337b7bb8c20cfba9566b4b83baf7cb81e0e07
amanverma-765 <akverma4aman@gmail.com>
2025-02-27 05:46:52 +0530

    completely remove kvault

--- coupit-app/composeApp/src/androidMain/kotlin/com/webxela/app/coupit/koin/Modules.android.kt
+++ coupit-app/composeApp/src/androidMain/kotlin/com/webxela/app/coupit/koin/Modules.android.kt
@@ -1,7 +1,5 @@
 package com.webxela.app.coupit.koin
 
-import com.liftric.kvault.KVault
-import com.russhwolf.settings.Settings
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.okhttp.OkHttp
 import org.koin.core.module.Module
@@ -10,10 +8,4 @@ import org.koin.dsl.module
 internal actual val platformModule: Module
     get() = module {
         single<HttpClientEngine> { OkHttp.create() }
-        single<KVault> {
-            KVault(
-                context = get(),
-                fileName = "coupit_securePrefs"
-            )
-        }
     }
--- coupit-app/composeApp/src/iosMain/kotlin/com/webxela/app/coupit/koin/Modules.ios.kt
+++ coupit-app/composeApp/src/iosMain/kotlin/com/webxela/app/coupit/koin/Modules.ios.kt
@@ -1,23 +1,11 @@
 package com.webxela.app.coupit.koin
 
-import com.liftric.kvault.KVault
-import com.russhwolf.settings.ExperimentalSettingsImplementation
-import com.russhwolf.settings.KeychainSettings
-import com.russhwolf.settings.Settings
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.darwin.Darwin
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
-@OptIn(ExperimentalSettingsImplementation::class)
 internal actual val platformModule: Module
     get() = module {
         single<HttpClientEngine> { Darwin.create() }
-        single<Settings> { KeychainSettings("coupit_securePrefs") }
-        single<KVault> {
-            KVault(
-                serviceName = "webxela",
-                accessGroup = "coupit_securePrefs"
-            )
-        }
     }
